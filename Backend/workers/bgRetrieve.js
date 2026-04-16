92380601f62027a547586ab40b504c255d09a606
aidanschooley <schooleyaidan7@gmail.com>
2026-04-16 15:32:38 -0400

    Update bgRetrieve worker

--- Backend/workers/bgRetrieve.js
+++ Backend/workers/bgRetrieve.js
@@ -7,10 +7,12 @@ console.log('Starting background retrieval task...');
 //Cron Minute Hour DayOfMonth Month DayOfWeek
 //Every 5 minutes: '*/5 * * * *'
 //Every minute: '* * * * *'
-cron.schedule('* * * * *', async () => {
+cron.schedule('*/5 * * * *', async () => {
     console.log('Running retrieval task every 5 minutes');
     const timestamp = new Date().toLocaleString();
     const dexcomData = await getCurrentBG();
+
+    
 
     const logMessage = `${timestamp} Current BG: ${dexcomData.value}\n`;
     console.log('Success: ' + logMessage);
