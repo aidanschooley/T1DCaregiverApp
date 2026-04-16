b8a7699029d0cf1ea324e467e7aa66857e6c39b0
aidanschooley <schooleyaidan7@gmail.com>
2026-04-16 15:42:27 -0400

    put background retrieval in a function and added it to server.js

--- backend/server.js
+++ backend/server.js
@@ -2,4 +2,5 @@ import 'dotenv/config.js'
 import app from './app.js'
 import bgRetrieve from './workers/bgRetrieve.js';
 
+backgroundRetrieve();
 app.listen(3000, () => console.log('Server running on port 3000'))
--- backend/workers/bgRetrieve.js
+++ backend/workers/bgRetrieve.js
@@ -3,20 +3,20 @@ import fs from 'fs';
 import { getCurrentBG } from '../services/dexcom/fetchLatestBg.js'
 import {classifyPriority} from '../constants/classifyPriority.js';
 
-console.log('Starting background retrieval task...');
-//Cron Minute Hour DayOfMonth Month DayOfWeek
-//Every 5 minutes: '*/5 * * * *'
-//Every minute: '* * * * *'
-cron.schedule('*/5 * * * *', async () => {
-    console.log('Running retrieval task every 5 minutes');
-    const timestamp = new Date().toLocaleString();
-    const dexcomData = await getCurrentBG();
 
-    
+function backgroundRetrieve() {
+    console.log('Starting background retrieval task...');
+    //Cron Minute Hour DayOfMonth Month DayOfWeek
+    //Every 5 minutes: '*/5 * * * *'
+    //Every minute: '* * * * *'
+    cron.schedule('*/5 * * * *', async () => {
+        console.log('Running retrieval task every 5 minutes');
+        const timestamp = new Date().toLocaleString();
+        const dexcomData = await getCurrentBG();
 
-    const logMessage = `${timestamp} Current BG: ${dexcomData.value}\n`;
-    console.log('Success: ' + logMessage);
+        const logMessage = `${timestamp} Current BG: ${dexcomData.value}\n`;
+        console.log('Success: ' + logMessage);
 });
+};
 
-
-export default bgRetrieve;
+export default backgroundRetrieve;
