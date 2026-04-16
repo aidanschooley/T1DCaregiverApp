98637c508b43aa930235bb5447cef7c00fe6ff1f
aidanschooley <schooleyaidan7@gmail.com>
2026-04-16 15:44:58 -0400

    renamed import for bgRetrieve in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import 'dotenv/config.js'
 import app from './app.js'
-import bgRetrieve from './workers/bgRetrieve.js';
+import backgroundRetrieve from './workers/bgRetrieve.js';
 
 backgroundRetrieve();
 app.listen(3000, () => console.log('Server running on port 3000'))
