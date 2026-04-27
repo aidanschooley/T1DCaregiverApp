b52824ceff8c3f50f1ce0886d2739406ba6af57e
aidanschooley <schooleyaidan7@gmail.com>
2026-04-27 19:07:36 -0400

    readFilesync and JSON.parse

--- backend/firebase/firebaseConfig.js
+++ backend/firebase/firebaseConfig.js
@@ -1,5 +1,10 @@
 import admin from 'firebase-admin';
-import serviceAccount from './glucosecare-d91fe-firebase-adminsdk-fbsvc-a8c1e9d7d9.json' assert { type: 'json' };
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const serviceAccount = JSON.parse(readFileSync(join(__dirname, './glucosecare-d91fe-firebase-adminsdk-fbsvc-a8c1e9d7d9.json'), 'utf8'));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
