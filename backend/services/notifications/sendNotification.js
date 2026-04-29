79a56ea5803a82e0f1411e9c1113742d026eda7e
aidanschooley <schooleyaidan7@gmail.com>
2026-04-28 20:02:20 -0400

    send notificaiton if urgent

--- backend/services/notifications/sendNotification.js
+++ backend/services/notifications/sendNotification.js
@@ -4,6 +4,6 @@ import sendNotificationFCM from '../../firebase/messaging.js';
 export async function sendNotification(userId, title, body, priority) {
   const fcmToken = await getToken(userId);
   if (!fcmToken) throw new Error(`No FCM token found for user ${userId}`);
-  const priorityNotification = ["P0", "P1", "P2", "P3"].includes(priority) ? "urgent" : "normal";
-  await sendNotificationFCM(fcmToken, title, body, priorityNotification);
+  const priorityNotification = ["P0", "P1", "P2"].includes(priority) ? "urgent" : "normal";
+  if (priorityNotification === "urgent") await sendNotificationFCM(fcmToken, title, body, priorityNotification);
 }
